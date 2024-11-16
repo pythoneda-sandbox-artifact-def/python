4ca043a12b9544344c90d798778105e2c7eba89b
Jose San Leandro Armendáriz <github@acm-sl.org>
2024-11-16 14:27:51 +0100

    Support for Python 3.12

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-shared-artifact-def/shared/0.0.63";
+      url = "github:pythoneda-shared-artifact-def/shared/0.0.64";
     };
     pythoneda-shared-git-shared = {
       inputs.flake-utils.follows = "flake-utils";
@@ -41,7 +41,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-shared-git-def/shared/0.0.54";
+      url = "github:pythoneda-shared-git-def/shared/0.0.55";
     };
     pythoneda-shared-nix-flake-shared = {
       inputs.flake-utils.follows = "flake-utils";
@@ -52,7 +52,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-shared-nix-flake-def/shared/0.0.59";
+      url = "github:pythoneda-shared-nix-flake-def/shared/0.0.61";
     };
     pythoneda-shared-pythonlang-banner = {
       inputs.flake-utils.follows = "flake-utils";
@@ -64,7 +64,7 @@
       inputs.nixos.follows = "nixos";
       inputs.pythoneda-shared-pythonlang-banner.follows =
         "pythoneda-shared-pythonlang-banner";
-      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.64";
+      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.70";
     };
   };
   outputs = inputs:
